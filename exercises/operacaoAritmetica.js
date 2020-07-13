b613b7363c49ba16cad9cdf4bdafb5f26fa31e7e
RenatoCostaDev <renatotrybe@gmail.com>
2020-07-13 17:52:45 -0300

    01- Exercício operações básicas

---
+++ exercises/operacaoAritmetica.js
@@ -0,0 +1,17 @@
+let a = 5;
+let b = 5;
+
+//Adição
+ console.log(a+b);
+
+//Subtracão
+console.log(a-b);
+
+//Multiplicação
+console.log(a*b);
+
+//Divisão
+console.log(a/b);
+
+//Módulo
+console.log(a%b);
