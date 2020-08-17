ef5e12005b462254cd4b4a7a2b72bd64d3385488
RenatoCostaDev <renatotrybe@gmail.com>
2020-08-17 09:01:39 -0300

    Iniciando conteúdos

---
+++ teste.js
@@ -0,0 +1,17 @@
+const productDetails = (firstProduct, secondProduct) => [
+  {
+    name: firstProduct,
+    details: {
+      productId: `${firstProduct}123`,
+    },
+  },
+  {
+    name: secondProduct,
+    details: {
+      productId: `${secondProduct}123`,
+    },
+  },
+];
+const retorno = productDetails('Camisa', 'Cueca');
+
+console.log(typeof [retorno[0],retorno[1]]);
---
+++ teste2.js
@@ -0,0 +1,61 @@
+/* eslint-disable no-var */
+/* eslint-disable block-scoped-var */
+/* eslint-disable no-redeclare */
+/* eslint-disable vars-on-top */
+
+/*
+  A função myCounter possui dois loops aninhados que inserem valores dentro de um array.
+  Como podemos perceber, eles vão adicionando valores ao array até sua condição de parada.
+  Corrija o código abaixo para que a função retorne o array correto.
+
+  Parâmetros:
+  - Nenhum.
+
+  Comportamento:
+  myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
+*/
+
+// const myCounter = () => {
+
+//   let myArray = [];
+//   do {
+//     if (myArray.length < 4) {
+//       for (var counter = 0; counter <= 3; counter += 1) {
+//         myArray.push(counter);
+//         for (var counter = 2; counter <= 3; counter += 1) {
+//           myArray.push(counter);
+//         }
+//       }
+//     }
+//     if (myArray.length < 7) {
+//       for (var counter = 1; counter <= 3; counter += 1) {
+//         myArray.push(counter);
+//         for (var counter = 2; counter <= 3; counter += 1) {
+//           myArray.push(counter);
+//         }
+//       }
+//     }
+//     if (myArray.length < 10) {
+//       for (var counter = 2; counter <= 3; counter += 1) {
+//         myArray.push(counter);
+//         for (var counter = 2; counter <= 3; counter += 1) {
+//           myArray.push(counter);
+//         }
+//       }
+//     }
+
+//   } while (myArray.length < 13);
+//   return myArray;
+// };
+
+const myCounter = () => {
+  var myArray = [];
+  for (var counter = 0; counter <= 3; counter += 1) {
+    myArray.push(counter);
+    for (var counter = 2; counter <= 3; counter += 1) {
+      myArray.push(counter);
+    }
+  }
+  return myArray;
+};
+console.log(myCounter());
